600a300febf8833228882467ee4342687918209e
yassou <senyass02@gmail.com>
2026-06-06 18:23:59 -0400

    Improved footer design

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,12 @@
+import Logo from "../assets/logo-footer.png";
+
 function Footer() {
     return (
-        <footer className="bg-slate-50 border-t border-slate-200">
+        <footer className="bg-pale-sky/50 border-t border-charcoal/10">
             <div className="max-w-7xl mx-auto px-6 py-8 flex flex-col md:flex-row items-center justify-between gap-3">
-                <h3 className="font-semibold text-[20px]"> Navy Dental </h3>
-                <p className="text-sm text-charcoal/80"> © 2026 Navy Dental • Designed by Yassmine Senouci</p>
+                <img src={Logo} alt="Dentist Website Logo" className="h-12 w-auto object-contain scale-110" />
+
+                <p className="text-center mt-2 !text-[18px] lg:!text-[20px] text-charcoal/80"> © 2026 Navy Dental • Designed by Yassmine Senouci</p>
             </div>
         </footer>
     );
